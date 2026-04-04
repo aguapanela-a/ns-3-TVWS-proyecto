86bc349589be1db183c50766417dbb6bbf70f235
Erick Buitrago <eribuit9@gmail.com>
2026-04-04 11:50:30 -0500

    comentarios para entender ejemplo inicial

--- scratch/Fusagasuga_base.cc
+++ scratch/Fusagasuga_base.cc
@@ -16,6 +16,7 @@
 #include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/internet-module.h"
+#include "ns3/netanim-module.h"
 #include "ns3/network-module.h"
 #include "ns3/point-to-point-module.h"
 
@@ -40,24 +41,31 @@ main(int argc, char* argv[])
     LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
     LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-    NodeContainer nodes;
+    //Dispositivos 2
+    NodeContainer nodes;  
     nodes.Create(2);
 
-    PointToPointHelper pointToPoint;
+    //cable punto a punto entre los dispositivos con velocidad de 5Mbps y retraso de 2ms
+    PointToPointHelper pointToPoint; 
     pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
+    //instalamos el canal punto a punto entre los nodos
     NetDeviceContainer devices;
     devices = pointToPoint.Install(nodes);
 
+    //instalamos el stack de internet en los nodos (para que puedan usar el protocolo IP)
     InternetStackHelper stack;
     stack.Install(nodes);
-
+    
+    //asignamos direcciòn IP y máscara a los dispositivos usando el helper Ipv4AddressHelper
     Ipv4AddressHelper address;
     address.SetBase("10.1.1.0", "255.255.255.0");
 
+    //asignamos direcciones IP a los dispositivos y obtenemos un contenedor de interfaces para usarlas posteriormente
     Ipv4InterfaceContainer interfaces = address.Assign(devices);
-
+    
+    //instalamos una aplicación de servidor de eco UDP en el nodo 1 para escuchar en el puerto 9 (echo protocol)
     UdpEchoServerHelper echoServer(9);
 
     ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
@@ -72,6 +80,10 @@ main(int argc, char* argv[])
     ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
     clientApps.Start(Seconds(2.0));
     clientApps.Stop(Seconds(10.0));
+
+    AnimationInterface anim("fusagasuga-anim.xml");
+    anim.SetConstantPosition(nodes.Get(0), 0, 50); //estaciòn base TVWS
+    anim.SetConstantPosition(nodes.Get(1), 100, 50);  //en CPE en la zona rural
 
     Simulator::Run();
     Simulator::Destroy();
