391d017e276c6dcf6cebf1f0b8ee7231c2145188
Erick Buitrago <eribuit9@gmail.com>
2026-04-06 00:43:40 -0500

    Adjust propagation model and CPE coordinates; update node sizes for better visualization

--- scratch/Fusagasuga_base.cc
+++ scratch/Fusagasuga_base.cc
@@ -51,7 +51,7 @@ main(int argc, char* argv[])
     double txRxSectorYaguiAntenna = 11.0; // Ganancia de transmisión  y recepción de la antena sectorial en dBi
     double sensitivity = -98.0; // Sensibilidad de recepción en dBm, acorde a las caracterìsticas del dispositivo Adaptrum TVWS para la banda UHF
     int frequency = 515; // Frecuencia de propagación de dispositivo en MHz
-    double pathLossExponent = 3.0; // Exponente de pérdida de propagación
+    double pathLossExponent = 0.0; // Exponente de pérdida de propagación
     int clientNodes = 3; // Cantidad de nodos cliente (CPEs) en la zona rural de Fusagasuga
 
 
@@ -85,9 +85,9 @@ main(int argc, char* argv[])
     //se asignan coordenadas a la estaciòn base TVWS y a los CPE en la zona rural de Fusagasuga
     positionAlloc->Add(Vector(0.0, 0.0, 0.0)); // Coordenadas para la estaciòn base TVWS
     
-    positionAlloc->Add(Vector(10000.0, 6000.0, 0.0)); // Coordenadas para el primer CPE
-    positionAlloc->Add(Vector(4000.0, 1000.0, 0.0)); // Coordenadas para el segundo CPE
-    positionAlloc->Add(Vector(7000.0, 7000.0, 0.0)); // Coordenadas para el tercer CPE
+    positionAlloc->Add(Vector(1000.0, 1000.0, 0.0)); // Coordenadas para el primer CPE
+    positionAlloc->Add(Vector(1300.0, 1500.0, 0.0)); // Coordenadas para el segundo CPE
+    positionAlloc->Add(Vector(160.0, 890.0, 0.0)); // Coordenadas para el tercer CPE
     
     //se asigna el posicionAlloc al helper de movilidad y se instala en la estaciòn base TVWS y los CPE en la zona rural de Fusagasuga
     mobility.SetPositionAllocator(positionAlloc);
@@ -246,13 +246,13 @@ main(int argc, char* argv[])
     //color del nodo de la base
     anim.UpdateNodeColor(baseStation.Get(0), 255, 0, 0); // Rojo para la estaciòn base TVWS
     //tamaño del nodo de la base
-    anim.UpdateNodeSize(baseStation.Get(0), 30, 30); // Tamaño más grande para la estaciòn base TVWS
+    anim.UpdateNodeSize(baseStation.Get(0), 80, 800); // Tamaño más grande para la estaciòn base TVWS
 
     for(uint32_t i = 0; i < ruralCPE.GetN(); ++i)
     {
         anim.UpdateNodeDescription(ruralCPE.Get(i), "CPE Rural");
         anim.UpdateNodeColor(ruralCPE.Get(i), 0, 255, 0); // Verde para los CPE
-        anim.UpdateNodeSize(ruralCPE.Get(i), 20, 20); // Tamaño más pequeño para los CPE
+        anim.UpdateNodeSize(ruralCPE.Get(i), 60, 60); // Tamaño más pequeño para los CPE
     }
     
     
